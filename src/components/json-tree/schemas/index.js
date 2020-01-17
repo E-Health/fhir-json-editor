f2889b077f974554414842737a4e25daa5d27347
beapen <github@gulfdoctor.net>
2020-01-17 18:25:25 -0500

    feat: Add Questionnaire Item Template

--- src/components/json-tree/schemas/index.js
+++ src/components/json-tree/schemas/index.js
@@ -6,6 +6,7 @@ import schemaObject from './object.json'
 import schemaString from './string.json'
 import jsonSchema from './json_schema.json'
 import sampleOrder from './sample_order.json'
+import questionnaireItem from './fhir_item.json'
 
 export default {
   'string': schemaString,
@@ -15,5 +16,6 @@ export default {
   'object': schemaObject,
   'array': schemaArray,
   'json_schema': jsonSchema,
+  'fhir_item': questionnaireItem,
   'sample_order': sampleOrder
 }
